0988b20e76235758622769c6fe006234daf742ef
Srybzx <129523638+Srybzx@users.noreply.github.com>
2024-01-07 00:54:49 +0500

    practicing string

--- practicing_string.py
+++ practicing_string.py
@@ -1,3 +1,30 @@
+# 1.0 code
 list_words = ["Thirty", "Days", "Of", "Python"]
 x = " ".join(list_words)
+print(x) 
+
+# 2.0 code
+list_case = ["Coding", "For", "All"]
+x = "".join(list_case)
 print(x)
+
+# 6.0
+txt = "hello my friends"
+x = txt.upper()
+print(x)
+
+# 7.0
+txt = "HELLO MY FRIENDS"
+x = txt.lower()
+print(x)
+
+# 11.0
+txt = "Coding For All"
+x = txt.replace("Coding", "Python")
+print(x)    
+
+# 14.0
+company = "Facebook, Google, Apple, IBM, Orcale, Amazon"
+x = company.split()
+print(x)
+
