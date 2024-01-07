a5bf1fa497e2f641b64c46f48df91cb080ca7425
Srybzx <129523638+Srybzx@users.noreply.github.com>
2024-01-07 17:12:18 +0500

    Practicing string

--- practicing_string.py
+++ practicing_string.py
@@ -8,23 +8,71 @@ list_case = ["Coding", "For", "All"]
 x = "".join(list_case)
 print(x)
 
-# 6.0
+# 3.0 code
+company = "Coding For All"
+
+# 4.0 code
+company = "Coding For All"
+print(company)
+
+# 5.0 code
+company = "Coding For All"      
+x = len(company)
+print(x)
+
+# 5.0 code with list
+mylist = ["Coding", "For", "All"]
+x = len(mylist)
+print(x)
+
+
+# 6.0 code
 txt = "hello my friends"
 x = txt.upper()
 print(x)
 
-# 7.0
+# 7.0 code
 txt = "HELLO MY FRIENDS"
 x = txt.lower()
 print(x)
 
-# 11.0
+# 8.0 code
+txt = "Coding For All"
+x = txt.swapcase()
+print(x)
+
+# 9.0 code
+txt = "Coding For All"
+x = txt.replace("Coding", "")
+print(x)
+
+# 10.0 code
+txt = "Coding For All"
+x = txt.find("Coding")
+print(x)
+
+# 11.0 code
 txt = "Coding For All"
 x = txt.replace("Coding", "Python")
 print(x)    
 
-# 14.0
+# 12.0 code
+txt = "Python For Everyone"
+x = txt.replace("Everyone", "All")
+print(x)
+
+# 13.0 code
+txt = "Coding For All"
+x = txt.split()
+print(x)
+
+# 14.0 code
 company = "Facebook, Google, Apple, IBM, Orcale, Amazon"
 x = company.split()
+print(x)
+
+# 17.0 code
+txt = "Coding For All"
+x = txt.index(" All")
 print(x)
 
